217f2f8d1831f6f1b08bf759ed3edee2eafc9a8a
takkanm <takkanm@gmail.com>
2017-05-18 17:53:57 +0900

    spike

--- lib/each_it.rb
+++ lib/each_it.rb
@@ -1,5 +1,17 @@
-require "each_it/version"
+require_relative "./each_it/version"
 
 module EachIt
-  # Your code goes here...
+  def each(*args, &block)
+    super(*args) do |i|
+      o = Object.new
+      o.define_singleton_method :method_missing, ->(name, *a, &b) { i.send(name, *a, &b) }
+      o.define_singleton_method :it, -> { i }
+      o.define_singleton_method :call, -> { instance_eval(&block) }
+      o.call
+    end
+  end
 end
+
+Enumerable.prepend EachIt
+Array.prepend EachIt
+Hash.prepend EachIt
